15001eac1b78de10a60590513f500736e0ebde12
SergiuScorbureanu <sergiu.scorbureanu@gmail.com>
2024-01-25 13:54:36 +0200

    Initial commit

---
+++ src/main/java/com/BodyBuddy/BodyBuddyAPI/BodyBuddyApiApplication.java
@@ -0,0 +1,13 @@
+package com.BodyBuddy.BodyBuddyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyBuddyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyBuddyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BodyBuddy/BodyBuddyAPI/BodyBuddyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BodyBuddy.BodyBuddyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyBuddyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
